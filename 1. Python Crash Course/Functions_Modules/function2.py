b86498e1ecc31692e230eeab9e344048c64ebb54
Kushal6554 <kushal9804485207@gmail.com>
2022-01-08 18:19:19 +0545

    Assignment as of Jan 08, 2022

---
+++ 1. Python Crash Course/Functions_Modules/function2.py
@@ -0,0 +1,16 @@
+import calculator
+# from calculator import *
+# from calculator import add
+# from calculator import subtract
+# from calculator import multiply
+# from calculator import divide
+# import calculator as cal
+
+a = 54
+b = 9
+
+r = calculator.add(a, b)
+r2 = calculator.subtract(a, b)
+
+print(f"The result of adding {a} and {b} is {r}")
+print(f"The result of subtracting {b} from {a} is {r2}")
---
+++ 1. Python Crash Course/The Basics/input-output.py
@@ -0,0 +1,7 @@
+# How to take input from use rin Python
+# Python gets user input with a message
+# Taking input from the user
+name = input("Enter your Name: ")
+
+# output
+print("Hello " + name)
